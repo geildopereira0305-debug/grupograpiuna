c97c706f22b8ae1f25483ac1f2f755e5f3688211
Felipe <systen3@gmail.com>
2026-08-08 16:58:03 -0300

    bug comentarios

--- src/components/TVPage.tsx
+++ src/components/TVPage.tsx
@@ -515,6 +515,7 @@ export const TVPage = () => {
                     type="text"
                     value={newMessage}
                     onChange={(e) => setNewMessage(e.target.value)}
+                    maxLength={500}
                     placeholder="Diga algo..."
                     className="flex-1 bg-gray-800 border border-gray-700 rounded-lg px-3 py-2 text-sm focus:outline-none focus:border-red-600 transition-colors"
                   />
